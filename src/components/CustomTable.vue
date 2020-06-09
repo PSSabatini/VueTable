0e8b837c932374e78bf7aeec1670d458c18522ca
PSSabatini <sabatinipola@gmail.com>
2020-06-09 10:49:18 +0200

    Clean files

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="custom-table">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomTable',
+  props: {
+  },
+};
+</script>
+
+<style lang="scss">
+  .custom-table {
+  }
+</style>
