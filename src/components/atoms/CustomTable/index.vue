fb2db3bd10bdeffa415c5bca02d0b2d251f12438
PSSabatini <sabatinipola@gmail.com>
2020-07-16 15:06:43 +0200

    Add array of objects as table data and render items in the table

--- src/components/atoms/CustomTable/index.vue
+++ src/components/atoms/CustomTable/index.vue
@@ -5,14 +5,12 @@
 
       <thead>
         <tr>
-
           <th
             v-for="(items, index) in columns"
             :key="index"
             @click="sort(items, $event)">
               {{ items }}
           </th>
-
         </tr>
       </thead>
 
@@ -41,7 +39,57 @@ export default {
 
   props: {
     itemsTable: {
-      default: '',
+      default: () => [],
+    },
+  },
+
+  computed: {
+    columns() {
+      const cols = [];
+
+      this.itemsTable.forEach((item) => {
+        const keys = Object.keys(item);
+
+        keys.forEach((key) => {
+          if (!cols.includes(key)) {
+            cols.push(key);
+          }
+        });
+      });
+
+      return cols;
+    },
+
+    sortedItems() {
+      const item = [];
+
+      this.itemsTable.forEach((it) => {
+        if (Object.keys(it)) {
+          const value = Object.values(it);
+
+          value.forEach((val, index) => {
+            // check for Tags
+            if (typeof val === 'string' && val.startsWith('&lt;') && val.includes('&lt;/')) {
+              const tag = val.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+              const parser = new DOMParser();
+              const tagHTML = parser.parseFromString(tag, 'text/html');
+
+              value[index] = tagHTML.body.innerHTML;
+            }
+
+            // check for Array
+            if (Array.isArray(val)) {
+              const arrToStr = val.join('<br>');
+
+              value[index] = arrToStr;
+            }
+          });
+
+          item.push(value);
+        }
+      });
+      console.log(item);
+      return item;
     },
   },
 };
--- src/components/atoms/CustomTable/story.js
+++ src/components/atoms/CustomTable/story.js
@@ -7,13 +7,54 @@ import CustomTable from './index.vue';
 // module readme
 import readme from './readme.md';
 
+const tableData = [
+  {
+    Name: 'Paola',
+    Age: 32,
+    Github: '<a target="_blank" href="www.github.com/PSSabatini">/PSSabatini</a>',
+    Technologies: ['Javascript', 'Vue.js', 'React', 'SCSS'],
+    'Developer Since': '01.05.2019',
+  },
+  {
+    Name: 'Leonardo',
+    Age: 33,
+    Github: '<a target="_blank" href="www.github.com/Carleon">/Carleon</a>',
+    Technologies: ['Javascript', 'Java', 'SQL', 'Jest'],
+    'Developer Since': '10.01.2000',
+  },
+  {
+    Name: 'Amanda',
+    Age: 45,
+    Github: '<a target="_blank" href="www.github.com/">/Amanda</a>',
+    Technologies: ['Phyton', 'Java'],
+    'Developer Since': '01.02.1995',
+  },
+  {
+    Name: 'Marija',
+    Age: 25,
+    Github: '<a target="_blank" href="www.github.com/">/Marija</a>',
+    Technologies: ['CSS', 'HTML5', 'Javascript'],
+    'Developer Since': '01.05.2019',
+  },
+  {
+    Name: 'Max',
+    Age: 31,
+    Github: '<a target="_blank" href="www.github.com/">/Max</a>',
+    Technologies: ['Vue.js', 'React', 'Angular'],
+    'Developer Since': '02.06.2015',
+  },
+];
+
 storiesOf('atoms/CustomTable', module)
   .add('default', () => ({
     components: { CustomTable },
     props: {
+      tableData: {
+        default: object('Table Data', tableData),
+      },
     },
     template: `
-          <custom-table>
+          <custom-table :itemsTable="tableData">
           </custom-table>
     `,
   }), {
